3a7d15a5ac6960ea4f94b54e6323e955898a6209
JayWilling <jwilling123@gmail.com>
2024-02-02 11:14:01 +1100

    Router setup
    installed three fiber, router dom, gh-pages, and set an initial landing page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styling/App.css";
+import { LandingPage } from "./pages/landingPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<header className="header">
+				<span>Blah</span>
+			</header>
+			<div className="routeContainer">
+				<Routes>
+					<Route element={<LandingPage />} path="/" />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LandingPage = () => {
+	return (
+		<div>
+			<span>This is the landing page.</span>
+		</div>
+	);
+};
